619d92ffef300374e46bc4b7d92da7ef3cc0b82c
MauroOyhanart <mauro.oyhanart@gmail.com>
2023-10-20 21:16:22 -0300

    usuario -paciente-medico no anda

--- back/src/main/java/com/metagiles/demometagiles/models/medico/Medico.java
+++ back/src/main/java/com/metagiles/demometagiles/models/medico/Medico.java
@@ -3,11 +3,13 @@ package com.metagiles.demometagiles.models.medico;
 import com.metagiles.demometagiles.models.usuario.Usuario;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 @Entity(name = "Medico")
 @Table(name = "medico")
+@DiscriminatorValue("M")
 public class Medico extends Usuario{
     @Column(
             name = "especialidad",
@@ -19,7 +21,8 @@ public class Medico extends Usuario{
 
     }
 
-    public Medico(String nombre, String apellido, String dni, String rol) {
+    public Medico(String nombre, String apellido, String dni, String rol, String especialidad) {
         super(nombre, apellido, dni, "medico");
+        this.especialidad = especialidad;
     }
 }
