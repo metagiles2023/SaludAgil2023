d80eb0e116004f6940ff27006b57813f0c53fb99
Mauro Oyhanart <mauro.oyhanart@gmail.com>
2023-10-23 14:43:43 -0300

    ver usuarios con ListaUsuarios

--- front/frontend-salud/src/app/page.js
+++ front/frontend-salud/src/app/page.js
@@ -3,13 +3,13 @@ import NavBar from '@/components/NavBar/NavBar';
 import { CartelDescripcion } from '@/components/carteles/CartelDescripcion';
 
 export default function Home() {
-  return (
+    return (
     <main className="flex flex-col">
-      <div>
-      <NavBar />
+        <div>
+        <NavBar />
         {/* Rest of your homepage content */}
-      </div>  
-      <CartelDescripcion mensaje="Pagina principal de Salud Agil 2023"/>
+        </div>  
+        <CartelDescripcion mensaje="Pagina principal de Salud Agil 2023"/>
     </main>
-  )
+    )
 }
