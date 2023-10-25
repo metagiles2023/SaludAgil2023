d995d39419a7168bd51f9f7ce5045fbad8888d15
MauroOyhanart <mauro.oyhanart@gmail.com>
2023-10-24 21:32:17 -0300

    formulario multiple

--- front/frontend-salud/src/components/SeleccionMultiples/FormularioMultiple.jsx
+++ front/frontend-salud/src/components/SeleccionMultiples/FormularioMultiple.jsx
@@ -1,21 +1,37 @@
 import React from 'react';
-import FormularioMedico from '../Medico/FormularioMedico';
-import FormularioPaciente from '../Paciente/FormularioPaciente';
+import Formulario from '../Formulario/Formulario';
 //import FormularioAdministrador from '../Administrador/FormularioAdministrador';
 
 const FormularioMultiple = ({ formulario }) => {
+    let fields = []
+    let url = ''
+    let tema = ''
     switch(formulario) {
         case "medico":
+            fields = ['nombre', 'apellido', 'dni', 'especialidad']
+            url = "/api/medico/create"
+            tema = "medico"
             return (
-                <FormularioMedico/>
+                <Formulario
+                fields={fields}
+                url={url}
+                tema={tema}
+                />
             )
         case "administrador":
             return (
                 <h1>este es el formulario de crear administrador</h1>
             )    
         case "paciente":
+            fields = ['nombre', 'apellido', 'dni', 'obra social']
+            url = "/api/paciente/create"
+            tema = "pirincuncun"
             return (
-                <FormularioPaciente/>
+                <Formulario
+                fields={fields}
+                url={url}
+                tema={tema}
+                />
             )
     }
 };
