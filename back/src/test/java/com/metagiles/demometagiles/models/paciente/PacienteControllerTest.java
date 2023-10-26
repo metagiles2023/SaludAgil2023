60fc4a3b5acd1c3804ca7362b80909b48443dfe1
LuciaBandeo <lubandeo@gmail.com>
2023-10-25 22:07:22 -0300

    se agregaron los unit tests

--- back/src/test/java/com/metagiles/demometagiles/models/paciente/PacienteControllerTest.java
+++ back/src/test/java/com/metagiles/demometagiles/models/paciente/PacienteControllerTest.java
@@ -1,23 +1,21 @@
 package com.metagiles.demometagiles.models.paciente;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-//import static org.mockito.junit.MockitoJUnitRunner.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class PacienteControllerTest {
 
@@ -34,6 +32,7 @@ public class PacienteControllerTest {
     }
 
     @Test
+    @DisplayName("Se simula el caso en el que se solicitan todos los pacientes")
     public void testGetAll() {
         // Simula el comportamiento de pacienteRepository.findAll()
         List<Paciente> pacientes = new ArrayList<>();
@@ -46,20 +45,7 @@ public class PacienteControllerTest {
     }
 
     @Test
-    public void testCreatePaciente() {
-        // Simula el comportamiento de pacienteRepository.existsBydni()
-        when(pacienteRepository.existsBydni(Mockito.anyString())).thenReturn(false);
-
-        // Crea una instancia de Paciente para la solicitud
-        Paciente request = new Paciente("John", "Doe", "12345678", "paciente");
-        request.setObraSocial("ObraSocial123");
-
-        // Llama al método createPaciente del controlador y verifica la respuesta
-        ResponseEntity<?> responseEntity = pacienteController.createPaciente(request);
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-    }
-
-    @Test
+    @DisplayName("Verifica que no se pueda crear un paciente si su dni ya existe")
     public void testCreatePacienteWhenDniExists() {
         // Simula el comportamiento de pacienteRepository.existsBydni() cuando el DNI ya existe
         when(pacienteRepository.existsBydni(Mockito.anyString())).thenReturn(true);
@@ -74,6 +60,7 @@ public class PacienteControllerTest {
     }
 
     @Test
+    @DisplayName("Verifica que no se pueda crear un paciente si el dni no es correcto")
     public void testCreatePacienteWithInvalidDni() {
         // Simula el comportamiento de pacienteRepository.existsBydni()
         when(pacienteRepository.existsBydni(Mockito.anyString())).thenReturn(false);
@@ -88,9 +75,10 @@ public class PacienteControllerTest {
     }
 
     @Test
+    @DisplayName("Verifica que no se pueda crear un paciente si se produce una excepcion")
     public void testCreatePacienteWithException() {
         // Simula una excepción en el repositorio
-        when(pacienteRepository.save(Mockito.any())).thenThrow(new RuntimeException("Simulated error"));
+        when(pacienteRepository.save(any())).thenThrow(new RuntimeException("Simulated error"));
 
         // Crea una instancia de Paciente para la solicitud
         Paciente request = new Paciente("John", "Doe", "12345678", "paciente");
