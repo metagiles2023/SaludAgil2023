0f71a70c4f14c32ba4a4040661075ef065a16c42
MauroOyhanart <mauro.oyhanart@gmail.com>
2023-10-28 16:09:43 -0300

    arreglando el merge

--- front/frontend-salud/src/app/page.js
+++ front/frontend-salud/src/app/page.js
@@ -1,17 +1,12 @@
-import Image from 'next/image'
-<<<<<<< HEAD
 import NavBar from '@/components/NavBar/NavBar';
 import { CartelDescripcion } from '@/components/carteles/CartelDescripcion';
-=======
-import NavBar from '../../components/NavBar';
->>>>>>> turnos-merge-a-main
+
 
 export default function Home() {
     return (
     <main className="flex flex-col">
         <div>
         <NavBar />
-        {/* Rest of your homepage content */}
         </div>  
         <CartelDescripcion mensaje="Pagina principal de Salud Agil 2023"/>
     </main>
