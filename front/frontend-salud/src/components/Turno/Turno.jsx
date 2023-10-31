4841c420a82a0fdb3f4535af7af38f269b1be4c6
MateoManolio <manoliomateo@gmail.com>
2023-10-31 18:26:57 -0300

    Arreglado el problema de el display cuando las flechas aparecen o no aparecen, esto también incluye la posibilidad de que las aparezcan según lógica que falta implementar

--- front/frontend-salud/src/components/Turno/Turno.jsx
+++ front/frontend-salud/src/components/Turno/Turno.jsx
@@ -4,9 +4,7 @@ const ListaTurno = ({ turnos }) => {
 
     if (!Array.isArray(turnos) || turnos.length === 0) {
         return (
-        <div className="flex items-center justify-center h-screen">
             <p className="text-2xl font-bold text-black">No hay turnos reservados</p>
-        </div>
         );
     }
 
