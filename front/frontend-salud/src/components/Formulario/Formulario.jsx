ddb4f527c8539e530a681faf4e476f26ff3330c6
Francisco Azqueta <franncisco1998@gmail.com>
2023-11-22 18:23:12 -0300

    tamaño entrada texto diagnostico

--- front/frontend-salud/src/components/Formulario/Formulario.jsx
+++ front/frontend-salud/src/components/Formulario/Formulario.jsx
@@ -3,6 +3,7 @@ import { skeleton } from '@nextui-org/react';
 import React, { useEffect, useState } from 'react';
 import DatePicker from 'react-datepicker';
 //Pido disculpas por lo que van a leer a continuacion
+//No pasa nada, pedimos perdon por el componente FichasMedicasFiltradas
 const Formulario = ({ fields, url, tema }) => {
     const initialFormData = {};
     fields.forEach((element) => {
@@ -245,23 +246,24 @@ const Formulario = ({ fields, url, tema }) => {
         return (
             <div key={i} className='mb-2'>
                 <label htmlFor={element} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{capFirst(element)}:</label>
-                <input
+                <textarea
                     type="text"
                     id={element}
                     name={element}
                     value={formData[element]}
                     onChange={handleChange}
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={`${capFirst(element)} del ${tema}`} required
+                    style={{ width: '1200px', height: '200px' }}
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-700 py-16 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={`${capFirst(element)} del ${tema}`} required
                 />
             </div>
         )
     };
 
     return (
-        <div className="text-xl text-black border border-gray-600 p-4">
+        <div className="text-xl text-black border border-gray-600 p-6">
             <form onSubmit={handleSubmit}>
                 {fields.map(renderField)}
-                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover-bg-blue-700 dark:focus:ring-blue-800">Crear {capFirst(tema)}</button>
+                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base sm:w-auto px-6 py-4 text-center dark:bg-blue-600 dark:hover-bg-blue-700 dark:focus:ring-blue-800">Crear {capFirst(tema)}</button>
             </form>
             <label>{textoBackend}</label>
         </div>
