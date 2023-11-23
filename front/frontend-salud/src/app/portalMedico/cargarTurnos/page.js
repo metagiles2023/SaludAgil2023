572dadf99eb6d67606c0441c3d06525a86b2ce72
SanchezV8 <fernandoagustinsanchez@hotmail.com>
2023-11-23 15:44:20 -0300

    Disenio de tabla para cargar turnos de a bloques

--- front/frontend-salud/src/app/portalMedico/cargarTurnos/page.js
+++ front/frontend-salud/src/app/portalMedico/cargarTurnos/page.js
@@ -1,23 +1,22 @@
 'use client';
 import Header from "@/components/Estructura/Header";
+import ContenidoTabla from "@/components/CargaDeTurnos/ContTablaRangoHorario";
 import Footer from "@/components/Estructura/Footer";
 import { useState } from 'react';
 import Box from '@mui/material/Box';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import Button from '@mui/material/Button';
 
 function dias(index) {
     return {
       id: `tab-${index}`,
       'aria-controls': `tabpanel-${index}`,
     };
-  }
+}
 
 export default function Turnos() {
     const [value, setValue] = useState(0);
-    
-    const handleChange = (event, newValue) => {
+    const handleChangeTabs = (event, newValue) => {
         setValue(newValue);
     };
     
@@ -26,9 +25,9 @@ export default function Turnos() {
             <Header />
             <main className="flex-1 flex justify-center items-center">
                 <div className="flex bg-white border-blue-500 border-[2px] rounded-[30px] overflow-hidden">
-                    <div className="flex flex-col "> 
-                        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                            <Tabs value={value} onChange={handleChange} aria-label="Tabla dias">
+                    <div className="flex flex-col pb-5"> 
+                        <Box sx={{ borderColor: 'divider' }}>
+                            <Tabs value={value} onChange={handleChangeTabs} aria-label="Tabla dias">
                                 <Tab className="font-roboto text-xl" label="Lunes" {...dias(0)} />
                                 <Tab className="font-roboto text-xl" label="Martes" {...dias(1)} />
                                 <Tab className="font-roboto text-xl" label="Miercoles" {...dias(2)} />
@@ -39,77 +38,55 @@ export default function Turnos() {
                             </Tabs>
                         </Box>
                         {value === 0 && (
-                            <div className="flex flex-col bg-green-100 m-5 h-[40vh]">
-                                    <div className="flex w-full h-full my-5 justify-center">
-                                        <div className="flex flex-col w-full h-full">
-                                            <div className="flex w-full h-12 justify-between px-20">
-                                                <div className="flex w-2/3 justify-center text-black bg-white rounded-[30px]">
-                                                    Desde
-                                                </div>
-                                                <div className="flex w-2/3 justify-center text-black bg-white rounded-[30px]">
-                                                    Desde
-                                                </div>
-                                            </div>
-                                        </div>
-                                        
-                                        <div className="flex w-8/12 content-between justify-between px-20">
-                                            <div className="flex w-2/3 h-12 justify-center text-black bg-white rounded-[30px]">
-                                                Desde
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="flex justify-end mb-5 mr-5"> 
-                                        <Button className="flex bg-green-500" variant="contained" color="success">
-                                            Guardar
-                                        </Button>
-                                    </div>
-                                </div>
+                            <div className="flex flex-col m-5">
+                                <ContenidoTabla />
+                            </div>
                         )}
 
                         {value === 1 && (
-                            <div className="bg-green-200 m-5 h-[40vh]">
+                            <div className="flex flex-col m-5">
                                 <h1 className="font-roboto text-xl">
-                                    holaaa 1
+                                    <ContenidoTabla />
                                 </h1>
                             </div>
                         )}
 
                         {value === 2 && (
-                            <div className="bg-green-300 m-5 h-[40vh]">
+                            <div className="flex flex-col m-5">
                                 <h1 className="font-roboto text-xl">
-                                    holaaa 2
+                                    <ContenidoTabla />
                                 </h1>
                             </div>
                         )}
 
                         {value === 3 && (
-                            <div className="bg-green-400 m-5 h-[40vh]">
+                            <div className="flex flex-col m-5">
                                 <h1 className="font-roboto text-xl">
-                                    holaaa 3
+                                    <ContenidoTabla />
                                 </h1>
                             </div>
                         )}
 
                         {value === 4 && (
-                            <div className="bg-green-500 m-5 h-[40vh]">
+                            <div className="flex flex-col m-5">
                                 <h1 className="font-roboto text-xl">
-                                    holaaa 4
+                                    <ContenidoTabla />
                                 </h1>
                             </div>
                         )}
 
                         {value === 5 && (
-                            <div className="bg-green-600 m-5 h-[40vh]">
+                            <div className="flex flex-col m-5">
                                 <h1 className="font-roboto text-xl">
-                                    holaaa 5
+                                    <ContenidoTabla />
                                 </h1>
                             </div>
                         )}
 
                         {value === 6 && (
-                            <div className="bg-green-700 m-5 h-[40vh]">
+                            <div className="flex flex-col m-5">
                                 <h1 className="font-roboto text-xl">
-                                    holaaa 6
+                                    <ContenidoTabla />
                                 </h1>
                             </div>
                         )}
---
+++ front/frontend-salud/src/components/CargaDeTurnos/ContTablaRangoHorario.jsx
@@ -0,0 +1,189 @@
+'use client';
+import Header from "@/components/Estructura/Header";
+import Footer from "@/components/Estructura/Footer";
+import { useState } from 'react';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Button from '@mui/material/Button';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import OutlinedInput from '@mui/material/OutlinedInput';
+
+const ITEM_HEIGHT = 100;
+const ITEM_PADDING_TOP = 16;
+function getStyles(hora, fecha, theme) {
+    return {
+      fontWeight:
+        fecha.indexOf(hora) === -1
+          ? theme.typography.fontWeightRegular
+          : theme.typography.fontWeightMedium,
+    };
+}
+
+const horas = [
+    '5:00',
+    '5:30',
+    '6:00',
+    '6:30',
+    '7:00',
+    '7:30',
+    '8:00',
+    '8:30',
+    '9:00',
+    '9:30',
+    '10:00',
+    '10:30',
+    '11:00',
+    '11:30',
+    '12:00',
+    '12:30',
+    '13:00',
+    '13:30',
+    '14:00',
+    '14:30',
+    '15:00',
+    '15:30',
+    '16:00',
+    '16:30',
+    '17:00',
+    '17:30',
+    '18:00',
+    '18:30',
+    '19:00',
+    '19:30',
+    '20:00',
+    '20:30',
+    '21:00',
+    '21:30',
+    '22:00',
+  ];
+
+  const duracionTurnos = [
+    '5',
+    '10',
+    '15',
+    '20',
+    '25',
+    '30',
+    '35',
+    '40',
+    '45',
+    '50',
+    '55',
+    '60',
+    '65',
+    
+  ];
+
+export default function Turnos() {
+    const MenuProps = {
+        PaperProps: {
+            style: {
+                maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+                width: 250,
+            },
+        },
+    };
+    
+    const theme = useTheme();
+    const [fechaDesde, setFechaDesde] = useState([]);
+    const handleChangeSelectDesde = (event) => {
+        setFechaDesde(event.target.value);
+    };
+    
+    const [fechaHasta, setFechaHasta] = useState([]);
+    const handleChangeSelectHasta = (event) => {
+        setFechaHasta(event.target.value);
+    };
+
+    const [turno, setDuracionTurno] = useState([]);
+    const handleChangeSelectDuracionTurno = (event) => {
+        setDuracionTurno(event.target.value);
+    };
+
+    return (
+        <div className="flex justify-around w-full h-full">
+            <div className="flex items-center -translate-y-4 gap-10 p-5">
+                <FormControl sx={{ width: 150 }}>
+                    <InputLabel id="label-fecha-desde">Desde</InputLabel>
+                    <Select
+                    labelId="select-fecha-desde"
+                    id="id-select-fecha-desde"
+                    multiple={false}
+                    value={fechaDesde}
+                    onChange={handleChangeSelectDesde}
+                    input={<OutlinedInput label="Hora" />}
+                    MenuProps={MenuProps}
+                    >
+                    {horas.map((hora) => (
+                        <MenuItem
+                        key={hora}
+                        value={hora}
+                        style={getStyles(hora, fechaDesde, theme)}
+                        >
+                        {hora}
+                        </MenuItem>
+                    ))}
+                    </Select>
+                </FormControl>
+                <FormControl sx={{ width: 150 }}>
+                    <InputLabel id="label-fecha-desde">Hasta</InputLabel>
+                    <Select
+                    labelId="select-fecha-desde"
+                    id="id-select-fecha-desde"
+                    multiple={false}
+                    value={fechaHasta}
+                    onChange={handleChangeSelectHasta}
+                    input={<OutlinedInput label="Hora" />}
+                    MenuProps={MenuProps}
+                    >
+                    {horas.map((hora) => (
+                        <MenuItem
+                        key={hora}
+                        value={hora}
+                        style={getStyles(hora, fechaHasta, theme)}
+                        >
+                        {hora}
+                        </MenuItem>
+                    ))}
+                    </Select>
+                </FormControl>
+            </div>
+            <div className="flex flex-col">
+                <div className="flex flex-grow items-center p-10">
+                    <FormControl sx={{ width: 150 }}>
+                        <InputLabel id="label-fecha-desde">Duracion</InputLabel>
+                        <Select
+                        labelId="select-fecha-desde"
+                        id="id-select-fecha-desde"
+                        multiple={false}
+                        value={turno}
+                        onChange={handleChangeSelectDuracionTurno}
+                        input={<OutlinedInput label="Turno" />}
+                        MenuProps={MenuProps}
+                        >
+                        {duracionTurnos.map((turno) => (
+                            <MenuItem
+                            key={turno}
+                            value={turno}
+                            style={getStyles(turno, fechaHasta, theme)}
+                            >
+                            {turno}
+                            </MenuItem>
+                        ))}
+                        </Select>
+                    </FormControl>
+                </div>
+                <div className="flex justify-end pr-5"> 
+                    <Button className="flex bg-green-500" variant="contained" color="success">
+                        Guardar
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
+}
